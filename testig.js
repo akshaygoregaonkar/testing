32bbe679bf8b8fe059a7b2d241f557fdcffe4100
Akshay Goregaonkar <akshay.goregaonkar@livehealth.in>
2022-06-18 12:44:18 +0530

    testing confilcts

--- testig.js
+++ testig.js
@@ -1,8 +1,13 @@
-const test = 10
-console.log(test)
-var a=10
-console.log("modified")
-const result =() =>{
-console.log("im in result function")
-}
-console.log("4th line")
+const test = 10;
+console.log(test);
+var a = 10;
+console.log("modified");
+
+const testFunction = () => {
+  console.log("4th line");
+};
+
+const result = () => {
+  console.log("im in result function");
+};
+console.log("4th line");
